08110fa6a8b2cc522bf5716ea1450a76e5bf02bb
sysu18364067 <luoganyuan629@163.com>
2021-06-01 15:46:52 +0800

    增加了记录文件提交功能

---
+++ upload_record_file.php
@@ -0,0 +1,19 @@
+<?php
+$json = array();
+if(isset($_POST['username'])){
+    $target_path  = "./user/record/".$_POST['username'].".jpg";//接收文件目录
+    if(move_uploaded_file($_FILES['record']['tmp_name'], $target_path)) {
+        $json['success'] = 1;
+        $json['message'] = "The file".  basename($_FILES['record']['name']). " has been uploaded";
+    }
+    else {
+        echo $_FILES["file"]["name"];
+        $json['success'] = 0;
+        $json['message'] = "Upload Failed";
+    }
+}
+else{
+    $json['success'] = 0;
+    $json['message'] = "Empty Username";
+}
+echo json_encode($json);
